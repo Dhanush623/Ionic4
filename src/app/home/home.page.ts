f33cbed09ddd097b6ed594b3678549464a93abdf
Dhanush <dhanush623@gmail.com>
2019-02-11 12:06:33 +0530

    Alert Box on Button Click

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { ToastController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +8,38 @@ import { ToastController } from '@ionic/angular';
 })
 export class HomePage {
 
-	constructor(public toastController: ToastController){
+	constructor(public toastController: ToastController, public alertController: AlertController){
 		// calling toast function
 		this.presentToast();
+	}
+
+	async presentAlertConfirm() {
+	    const alert = await this.alertController.create({
+	      header: 'Alert Header!',
+	      message: 'This is sample for <strong>ALert</strong> Box!!!',
+	      buttons: [
+	        {
+	          text: 'Cancel',
+	          role: 'cancel',
+	          cssClass: 'secondary',
+	          handler: () => {
+	            console.log('Confirm Cancel');
+	          }
+	        }, {
+	          text: 'Okay',
+	          handler: () => {
+	            console.log('Confirm Okay');
+	          }
+	        }
+	      ]
+	    });
+	    await alert.present();
 	}
 
 	// toast must use async and await in ionic.
 	async presentToast() {
 	    const toast = await this.toastController.create({
-	      message: 'Enter Valid Details...',
+	      message: 'Wecome...!',
 	      duration: 2000,
 	      position: 'top'
 	    });
