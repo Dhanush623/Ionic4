d3905ef184f49af49f8632b1e3d7ed60a6c020d9
Dhanush <dhanush623@gmail.com>
2019-02-11 11:33:44 +0530

    Toast Function Added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+	constructor(public toastController: ToastController){
+		// calling toast function
+		this.presentToast();
+	}
+
+	// toast must use async and await in ionic.
+	async presentToast() {
+	    const toast = await this.toastController.create({
+	      message: 'Enter Valid Details...',
+	      duration: 2000,
+	      position: 'top'
+	    });
+	    toast.present();
+	}
 
 }
